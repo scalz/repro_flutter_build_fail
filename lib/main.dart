c9e7558a5d3293e6a04876d144460577a644b0b8
scalz <scalz.my49@gmail.com>
2024-04-23 12:10:44 +0200

    Repro flutter build failed when using state_beacon

--- lib/main.dart
+++ lib/main.dart
@@ -33,9 +33,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late final controller = FilterSearchController<String>(
     futureFetch: fetchDatas,
-    filter: (datas, text) => datas.contains(text),
-    compareAsc: (a, b) => a.compareTo(b),
-    compareDesc: (a, b) => b.compareTo(a),
   );
 
   Future<List<String>> fetchDatas() async {
@@ -64,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
               AsyncData<List<String>>(value: final datas) => Text(
                   datas.isEmpty
                       ? 'no data'
-                      : controller.filteredDatas.value.toString(),
+                      : controller.datas.value.toString(),
                   style: Theme.of(context).textTheme.headlineMedium,
                 ),
               AsyncError(error: final e) => Text('Error $e'),
@@ -80,22 +77,9 @@ class _MyHomePageState extends State<MyHomePage> {
 class FilterSearchController<T> extends BeaconController {
   FilterSearchController({
     required this.futureFetch,
-    required this.filter,
-    required this.compareAsc,
-    required this.compareDesc,
   });
 
   final Future<List<T>> Function()? futureFetch;
-
-  final bool Function(T, String)? filter;
-
-  final int Function(T, T)? compareAsc;
-
-  final int Function(T, T)? compareDesc;
-
-  final searchTextCtrl = TextEditingController();
-
-  late final searchText = B.writable('');
 
   late final datas = B.list<T>([]);
 
@@ -104,31 +88,9 @@ class FilterSearchController<T> extends BeaconController {
     return datas.value;
   });
 
-  late final sortedAsc = B.writable(true);
-
-  late final filteredDatas = B.derived<List<T>>(() {
-    final text = searchText.value;
-    if (text.isEmpty) return datas.value;
-
-    return datas.value
-        .where((data) => filter?.call(data, text) ?? true)
-        .toList();
-  });
-
-  late final sortedDatas = B.derived<List<T>>(() {
-    final result = filteredDatas.value;
-    if (sortedAsc.value) {
-      result.sort(compareAsc);
-    } else {
-      result.sort(compareDesc);
-    }
-    return result;
-  });
-
   @override
   void dispose() {
     debugPrint('dispose FilterSearchController');
     super.dispose();
-    searchTextCtrl.dispose();
   }
 }
